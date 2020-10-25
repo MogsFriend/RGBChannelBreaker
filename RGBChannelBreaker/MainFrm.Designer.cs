f25c0e9963c616ddf1b40be655a8e6a9a0dc7263
MogsFriend <antares@ffxiv.me>
2020-10-25 22:04:33 +0900

    upload project

---
+++ RGBChannelBreaker/MainFrm.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace RGBChannelBreaker
+{
+    partial class MainFrm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainFrm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.파일ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.열기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.저장ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.Blue = new System.Windows.Forms.PictureBox();
+            this.Green = new System.Windows.Forms.PictureBox();
+            this.Red = new System.Windows.Forms.PictureBox();
+            this.Alpha = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Blue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Green)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Red)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Alpha)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.White;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(752, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 파일ToolStripMenuItem
+            // 
+            this.파일ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.열기ToolStripMenuItem,
+            this.저장ToolStripMenuItem});
+            this.파일ToolStripMenuItem.Name = "파일ToolStripMenuItem";
+            this.파일ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.파일ToolStripMenuItem.Text = "파일";
+            // 
+            // 열기ToolStripMenuItem
+            // 
+            this.열기ToolStripMenuItem.Name = "열기ToolStripMenuItem";
+            this.열기ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.열기ToolStripMenuItem.Text = "열기";
+            this.열기ToolStripMenuItem.Click += new System.EventHandler(this.FileOpenMenu);
+            // 
+            // 저장ToolStripMenuItem
+            // 
+            this.저장ToolStripMenuItem.Name = "저장ToolStripMenuItem";
+            this.저장ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.저장ToolStripMenuItem.Text = "저장";
+            this.저장ToolStripMenuItem.Click += new System.EventHandler(this.FileSaveMenu);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 24);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(752, 568);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.AutoScroll = true;
+            this.tabPage1.Controls.Add(this.Alpha);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(0);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(744, 542);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "A";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.AutoScroll = true;
+            this.tabPage2.Controls.Add(this.Red);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(0);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(744, 542);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "R";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.AutoScroll = true;
+            this.tabPage3.Controls.Add(this.Green);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(0);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(744, 542);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "G";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.AutoScroll = true;
+            this.tabPage4.Controls.Add(this.Blue);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Margin = new System.Windows.Forms.Padding(0);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(744, 542);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "B";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.Blue.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.Blue.Location = new System.Drawing.Point(0, 0);
+            this.Blue.Name = "pictureBox1";
+            this.Blue.Size = new System.Drawing.Size(8, 8);
+            this.Blue.TabIndex = 0;
+            this.Blue.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.Green.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.Green.Location = new System.Drawing.Point(0, 0);
+            this.Green.Name = "pictureBox2";
+            this.Green.Size = new System.Drawing.Size(8, 8);
+            this.Green.TabIndex = 1;
+            this.Green.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.Red.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.Red.Location = new System.Drawing.Point(0, 0);
+            this.Red.Name = "pictureBox3";
+            this.Red.Size = new System.Drawing.Size(8, 8);
+            this.Red.TabIndex = 1;
+            this.Red.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.Alpha.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.BackgroundImage")));
+            this.Alpha.Location = new System.Drawing.Point(0, 0);
+            this.Alpha.Name = "pictureBox4";
+            this.Alpha.Size = new System.Drawing.Size(8, 8);
+            this.Alpha.TabIndex = 1;
+            this.Alpha.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 592);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Blue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Green)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Red)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Alpha)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 파일ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 열기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 저장ToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.PictureBox Alpha;
+        private System.Windows.Forms.PictureBox Red;
+        private System.Windows.Forms.PictureBox Green;
+        private System.Windows.Forms.PictureBox Blue;
+    }
+}
+
---
+++ RGBChannelBreaker/MainFrm.cs
@@ -0,0 +1,76 @@
+﻿namespace RGBChannelBreaker
+{
+    using System;
+    using System.Drawing;
+    using System.IO;
+    using System.Windows.Forms;
+
+    public partial class MainFrm : Form
+    {
+        public MainFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void FileOpenMenu(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Filter = "PNG|*.png";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if (File.Exists(ofd.FileName))
+                    {
+                        var img = (Bitmap)Image.FromFile(ofd.FileName);
+
+                        Blue.Size = img.Size;
+                        Green.Size = img.Size;
+                        Red.Size = img.Size;
+                        Alpha.Size = img.Size;
+
+                        Bitmap A = new Bitmap(img.Width, img.Height);
+                        Bitmap R = new Bitmap(img.Width, img.Height);
+                        Bitmap G = new Bitmap(img.Width, img.Height);
+                        Bitmap B = new Bitmap(img.Width, img.Height);
+
+                        for (var x = 0; x < img.Width; x++)
+                        {
+                            for (var y = 0; y < img.Height; y++)
+                            {
+                                var a = img.GetPixel(x, y);
+                                A.SetPixel(x, y, Color.FromArgb(a.A, 255, 255, 255));
+                                R.SetPixel(x, y, Color.FromArgb(a.R, 255, 255, 255));
+                                G.SetPixel(x, y, Color.FromArgb(a.G, 255, 255, 255));
+                                B.SetPixel(x, y, Color.FromArgb(a.B, 255, 255, 255));
+                            }
+                        }
+
+                        Alpha.Image = A;
+                        Red.Image = R;
+                        Green.Image = G;
+                        Blue.Image = B;
+                    }
+                }
+                catch 
+                {
+                    MessageBox.Show("이미지 처리 과정에서 문제가 발생했습니다.");
+                    Console.WriteLine("이미지 처리 오류");
+                }
+            }
+        }
+
+        private void FileSaveMenu(object sender, EventArgs e)
+        {
+            var sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                var filename = sfd.FileName;
+                Alpha.Image.Save(filename.Replace(".png", "_A.png"));
+                Red.Image.Save(filename.Replace(".png", "_R.png"));
+                Green.Image.Save(filename.Replace(".png", "_G.png"));
+                Blue.Image.Save(filename.Replace(".png", "_B.png"));
+            }
+        }
+    }
+}
